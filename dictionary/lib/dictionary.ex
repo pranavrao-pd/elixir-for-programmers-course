39d9af1b422668256deadfd6be5156ea3a7a2795
Pranav Rao <prao@pagerduty.com>
2021-10-31 15:19:23 -0700

    Added credo

--- dictionary/lib/dictionary.ex
+++ dictionary/lib/dictionary.ex
@@ -1,10 +1,11 @@
 defmodule Dictionary do
-
+  @spec random_word :: any
   def random_word() do
     word_list()
     |> Enum.random()
   end
 
+  @spec word_list :: [binary]
   def word_list do
     "../assets/words.txt"
     |> Path.expand(__DIR__)
--- dictionary/lib/lists.ex
+++ dictionary/lib/lists.ex
@@ -1,24 +1,31 @@
 defmodule Lists do
+  @spec len(list) :: non_neg_integer
   def len([]), do: 0
   def len([_h | t]), do: 1 + len(t)
+  @spec sum([number]) :: number
   def sum([]), do: 0
   def sum([h | t]), do: h + sum(t)
 
+  @spec double([number]) :: [number]
   def double([]), do: []
   def double([h | t]), do: [2 * h | double(t)]
 
+  @spec square([number]) :: [number]
   def square([]), do: []
   def square([h | t]), do: [h * h | square(t)]
 
+  @spec map(list, any) :: list
   def map([], _func), do: []
 
   def map([h | t], func) do
     [func.(h) | map(t, func)]
   end
 
+  @spec sum_pairs([number]) :: [number]
   def sum_pairs([]), do: []
   def sum_pairs([h1, h2 | t]), do: [h1 + h2 | sum_pairs(t)]
 
+  @spec even_length?(list) :: boolean
   def even_length?([]), do: true
   def even_length?([_ | []]), do: false
   def even_length?([_, _ | t]), do: even_length?(t)
